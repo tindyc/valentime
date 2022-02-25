57e232b41c5a2949438e0f3fabcfa9da5953ca3a
lseparatio <consultantaanglia@gmail.com>
2022-02-25 15:02:37 +0000

    Add hearts on click effect

---
+++ assets/js/script.js
@@ -0,0 +1,100 @@
+let onn = "1"
+
+if (onn === "1"){
+    document.onmousedown = function(e) {
+        down = true;
+        event = e;
+        console.log(e)
+    }
+}
+var brd = document.createElement("DIV");
+document.body.insertBefore(brd, document.getElementById("heart"));
+
+const duration = 3000;
+const speed = 0.5;
+const cursorXOffset = 0;
+const cursorYOffset = -5;
+
+var hearts = [];
+		
+function generateHeart(x, y, xBound, xStart, scale)
+{
+	var heart = document.createElement("DIV");
+	heart.setAttribute('class', 'heart');
+	brd.appendChild(heart);
+	heart.time = duration;
+	heart.x = x;
+	heart.y = y;
+	heart.bound = xBound;
+	heart.direction = xStart;
+	heart.style.left = heart.x + "px";
+	heart.style.top = heart.y + "px";
+	heart.scale = scale;
+	heart.style.transform = "scale(" + scale + "," + scale + ")";
+	if(hearts == null)
+		hearts = [];
+	hearts.push(heart);
+	return heart;
+}
+
+var down = false;
+var event = null;
+
+document.onmouseup = function(e) {
+	down = false;
+}
+
+document.onmousemove = function(e) {
+	event = e;
+}
+
+document.ontouchstart = function(e) {
+	down = true;
+	event = e.touches[0];
+}
+
+document.ontouchend = function(e) {
+	down = false;
+}
+
+document.ontouchmove = function(e) {
+	event = e.touches[0];
+}
+
+var before = Date.now();
+var id = setInterval(frame, 5);
+var gr = setInterval(check, 100);
+
+function frame()
+{
+	var current = Date.now();
+	var deltaTime = current - before;
+	before = current;
+	for(i in hearts)
+	{
+		var heart = hearts[i];
+		heart.time -= deltaTime;
+		if(heart.time > 0)
+		{
+			heart.y -= speed;
+			heart.style.top = heart.y + "px";
+			heart.style.left = heart.x + heart.direction * heart.bound * Math.sin(heart.y * heart.scale / 30) / heart.y * 100 + "px";
+    }
+		else
+		{
+			heart.parentNode.removeChild(heart);
+			hearts.splice(i, 1);
+		}
+	}
+}
+
+function check()
+{
+	if(down)
+	{
+		var start = 1 - Math.round(Math.random()) * 2;
+		var scale = Math.random() * Math.random() * 0.8 + 0.2;
+		var bound = 30 + Math.random() * 20;
+		generateHeart(event.pageX - brd.offsetLeft + cursorXOffset, event.pageY - brd.offsetTop + cursorYOffset, bound, start, scale);
+	}
+}
