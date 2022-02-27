aef4f5835cde92f4b7294b5673d71ca406a97793
KelvenH <kelvenhughes@me.com>
2022-02-27 17:06:17 +0000

    Create new card library and function to make random selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,78 @@
-
+// Card Data Sets 
+const const cardData = {
+	gameSolo: [{
+		title: "Club Penguin",
+		url: "https://cprewritten.net/",
+		image: "assets/images/noimage.png",
+		label: "Free to play - make new friends and solve puzzles together in this frozen virtual world"
+	},
+	{
+		title: "MiniClip",
+		url: "https://www.miniclip.com/games/genre-2/multiplayer/en/",
+		image: "assets/images/noimage.png",
+		label: "Large selection of multiplayer games available on desktop and mobile. Make new friends as you team up for a challenge."
+	},
+	{
+		title: "Best Games to Meet People Online",
+		url: "https://www.esquireme.com/gaming/44920-lonely-these-are-the-best-games-to-make-friends-online",
+		image: "assets/images/noimage.png",
+		label: "Some of the best games with communities to meet new people"
+	}],
+ 	gameCouples: [{
+		title: "Guess the Song (Android)",
+		url: "  https://play.google.com/store/apps/details?id=quess.song.music.pop.quiz",
+		image: "",
+		label: " Guess the Song is a fun mobile game where you can go head-to-head in a battle to see who can identify songs faster"
+	},
+	{
+		title: "Steamy Role Play",
+		url: "https://www.thedatingdivas.com/10-super-steamy-stories-for-roleplay-romance/",
+		image: "",
+		label: "Roleplay is an exciting way to add some spice, grab some props and bring your fantasy scenarios to life!"
+	},
+	{	
+		title: "Sexy Dice Game",
+		url: "https://www.thedatingdivas.com/sexy-dice-intimate-date-night/",
+		image: "",
+		label: "Give this free printable sexy dice game a roll and see just how fun an intimate date night can be."
+	},
+	{
+		title: "Sexy Tic Tac Toe",
+		url: "https://www.thedatingdivas.com/sexy-tic-tac-toe/",
+		image: "",
+		label: "Spice things up with this spin on an old classic. With steamy action cards no matter who wins the game you'll both be winners tonight!"
+	},
+	{
+		title: "Romantic Games for Couples",
+		url: "https://www.marriage.com/advice/romance/fun-and-romantic-games-for-couples/",
+		image: "",
+		label: "Rid the monotonous routine with some fun romantic games that are easy to play and a great way to spice things up."
+	},
+	{
+		title: "21 Fun Games to Play at home",
+		url: "https://www.thedatingdivas.com/sexy-tic-tac-toe/",
+		image: "https://www.stylecraze.com/articles/games-for-couples/",
+		label: "Suggestions for couples to interact in new and unique ways to strengthen your connections."
+	}],
+ 	gameOnline: [{
+		title: "",
+		url: "",
+		image: "",
+		label: ""
+	},
+	{
+		title: "",
+		url: "",
+		image: "",
+		label: ""
+	},
+	{
+		title: "",
+		url: "",
+		image: "",
+		label: ""
+	}],
+};
 
 //Facebook SDK JS
 
@@ -16,11 +90,62 @@
 
 const cards = document.querySelectorAll(".cards__single");
 
-function flipCard() {
+function flipCard(e) {
+  // fip card animation	
   this.classList.toggle("flip");
-}
-cards.forEach((card) => card.addEventListener("click", flipCard));
+  
+  // get parent ID (if user clicks on H3 or image)
+  var target = e.target;
+  console.log("target", target);
 
+  var elParentId = target.parentNode.id;
+  console.log("elParentID1", elParentId);
+
+  // get grand-parent id (if user clicks on text)
+  var nodeName = e.target.nodeName;
+  console.log("nodename", nodeName);
+
+  if (nodeName == "P") {
+	  elParentId = target.parentNode.parentNode.id;
+  	}
+  
+  console.log("elParentID2", elParentId);
+  //pass ID to randomCard function
+  randomCard(elParentId);
+}
+
+// choose a random card matching the id to the relevant data set
+// create empty array
+let allCardChoices = {}
+
+function randomCard(elParentId) {
+	if (elParentId == "game-single") {
+		console.log("random from game-single");
+		allCardChoices = cardData.gameSolo;
+	}
+	console.log("show card choices", allCardChoices);
+	
+	// add if statements here for all cardIDs and map to correct data set
+
+	// make random seletion from allCards array
+	let randomSelection = allCardChoices[Math.random() * allCardChoices.length | 0];
+	console.log("random selection", randomSelection);
+	
+	// populate back of card with random selection
+	let cardTitle = randomSelection.title;
+	console.log("card title", cardTitle);
+	let cardURL = randomSelection.url;
+	console.log("card url", cardURL);
+	let cardImg = randomSelection.image;
+	console.log("card img", cardImg);
+	let cardLabel = randomSelection.label;
+	console.log("card label", cardLabel);
+
+}
+
+
+
+cards.forEach((card) => card.addEventListener("click", flipCard, false));
 
 
 //Hearts on click code
